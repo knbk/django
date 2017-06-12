fe6a9f34284188fb88b8b018152dfb039a45eb4c
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2017-06-12 05:44:52 +0200

    Remove reference to check_framework.urls.contains_tuple.

--- tests/urlpatterns/tests.py
+++ tests/urlpatterns/tests.py
@@ -5,8 +5,7 @@ from django.urls import resolve
 
 
 class InvalidURLsTests(SimpleTestCase):
-    @override_settings(ROOT_URLCONF='urlpatterns.urls.contains_tuple')
     def test_contains_tuple_not_url_instance(self):
         with self.assertRaises(ImproperlyConfigured):
-            with override_settings(ROOT_URLCONF='check_framework.urls.contains_tuple'):
+            with override_settings(ROOT_URLCONF='urlpatterns.urls.contains_tuple'):
                 resolve('/')
